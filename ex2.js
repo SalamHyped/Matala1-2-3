ad4384480ce899a0b10bd23c8cee45d723b8adca
SalamHyped <shibli.salam30@gmail.com>
2024-12-02 23:57:20 +0200

    Add files via upload

--- ex2.js
+++ ex2.js
@@ -1,6 +1,7 @@
-let num = [50, 40, 2, 0, 8, 4, 0, 90, 87, 0, 99, 0, 0, 45, 25, 28, 67];
-let count = 0;
-for(let i=0;i<num.length;i++)
- count+=num[i]==0 ? 1:0
-
-console.log(zeros);
+for (let num = 2; num < 237; num++) {
+  let flag = true;
+  for (let j = 2; j < num; j++) {
+    if (num % j == 0) flag = false;
+  }
+  if (flag) console.log(num);
+}
